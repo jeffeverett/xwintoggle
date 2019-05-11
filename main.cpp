7c47eab60d13e228e3ce80361cfd80dc58119746
Jeff Everett <everett.jeff.w@gmail.com>
2019-05-11 02:26:30 -0600

    Recursive checks + cmdLine option

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <string>
 #include <cstring>
+#include <sstream>
 
 #define _NET_WM_STATE_REMOVE 0
 #define _NET_WM_STATE_ADD    1
@@ -17,22 +18,24 @@
 Display *display;
 Window rootWindow;
 
-
-std::vector<Window> findWindowsOwnedByExe(std::string exeName)
+void findWindowsOwnedByExeRecursive(std::string exeName, bool useCmdLine, Window startWindow, std::vector<Window> &windows)
 {
-  std::vector<Window> windows;
-
   // Loop through all children of root window and check whether the executable names
   // associated with the given PID match the passed in executable name
   Atom pidAtom = XInternAtom(display, "_NET_WM_PID", true);
+  Window rWindow;
   Window parentWindow;
   Window *childrenWindows;
   unsigned int numChildren;
-  XQueryTree(display, rootWindow, &rootWindow, &parentWindow, &childrenWindows, &numChildren);
+  XQueryTree(display, startWindow, &rWindow, &parentWindow, &childrenWindows, &numChildren);
   #ifdef DEBUG
-    std::cout << numChildren << " children of root window" << std::endl;
+    std::cout << numChildren << " children of window 0x";
+    std::ios_base::fmtflags f(std::cout.flags());
+    std::cout << std::hex << startWindow << std::endl;
+    std::cout.flags(f);
   #endif
   for (int i = 0; i < numChildren; i++) {
+    // Determine if this window matches desired criteria
     Atom type;
     int format;
     unsigned long numItems;
@@ -41,29 +44,56 @@ std::vector<Window> findWindowsOwnedByExe(std::string exeName)
     if (XGetWindowProperty(display, childrenWindows[i], pidAtom, 0, 1, false, AnyPropertyType,
       &type, &format, &numItems, &bytesAfter, &pidProp) == Success && pidProp != 0) {
       unsigned long pid = *((unsigned long *)pidProp);
-      std::string filename = "/proc/" + std::to_string(pid) + "/exe";
-      char *exeFile = realpath(filename.c_str(), nullptr);
-      if (exeFile) {
-        if (strcmp(exeFile, exeName.c_str()) == 0) {
-          #ifdef DEBUG
-            std::cout << "found window with PID " << pid;
-            std::ios_base::fmtflags f(std::cout.flags());
-            std::cout << " and ID 0x" << std::hex << childrenWindows[i] << "." << std::endl;
-            std::cout.flags(f);
-          #endif
-          windows.push_back(childrenWindows[i]);
+      if (useCmdLine) {
+        std::string filename = "/proc/" + std::to_string(pid) + "/cmdline";
+        std::ifstream cmdlineFile(filename);
+        if (cmdlineFile.is_open()) {
+          std::stringstream ss;
+          ss << cmdlineFile.rdbuf();
+          std::string cmdline = ss.str();
+          if (cmdline.find(exeName) != -1) {
+            #ifdef DEBUG
+              std::cout << "found window with PID " << pid;
+              std::ios_base::fmtflags f(std::cout.flags());
+              std::cout << " and ID 0x" << std::hex << childrenWindows[i] << "." << std::endl;
+              std::cout.flags(f);
+            #endif
+            windows.push_back(childrenWindows[i]);
+          }
         }
-
-        free(exeFile);
       }
+      else {
+        std::string filename = "/proc/" + std::to_string(pid) + "/exe";
+        char *exeFile = realpath(filename.c_str(), nullptr);
+        if (exeFile) {
+          if (strcmp(exeFile, exeName.c_str()) == 0) {
+            #ifdef DEBUG
+              std::cout << "found window with PID " << pid;
+              std::ios_base::fmtflags f(std::cout.flags());
+              std::cout << " and ID 0x" << std::hex << childrenWindows[i] << "." << std::endl;
+              std::cout.flags(f);
+            #endif
+            windows.push_back(childrenWindows[i]);
+          }
 
+          free(exeFile);
+        }
+      }
       XFree(pidProp);
     }
+
+    // Recursively check children
+    findWindowsOwnedByExeRecursive(exeName, useCmdLine, childrenWindows[i], windows);
   }
   if (childrenWindows) {
     XFree(childrenWindows);
   }
+}
 
+std::vector<Window> findWindowsOwnedByExe(std::string exeName, bool useCmdLine)
+{
+  std::vector<Window> windows;
+  findWindowsOwnedByExeRecursive(exeName, useCmdLine, rootWindow, windows);
   return windows;
 }
 
@@ -72,7 +102,7 @@ void handleKeypress(std::string exeName)
   Atom stateAtom = XInternAtom(display, "WM_STATE", False);
 
   // Determine if there is a window owned by the given executable
-  auto windows = findWindowsOwnedByExe(exeName);
+  auto windows = findWindowsOwnedByExe(exeName, true);
 
   for (auto window : windows) {
     Atom type;
